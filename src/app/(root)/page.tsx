6408cb5ac5fd5d978155411211856afaab13d4a7
Anees Asghar <aneesasgharsyed@gmail.com>
2023-08-19 23:20:50 +0100

    Define project folder structure

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-slate-900 text-white flex justify-center items-center">
+      <h1 className="text-4xl">SINFO WEBAPP</h1>
+    </main>
+  )
+}
