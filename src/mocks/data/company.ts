fad8234ca66ed3795b00c6d16162de5b487c5383
Anees Asghar <aneesasgharsyed@gmail.com>
2024-11-23 11:44:04 +0000

    integrate mock service worker (#18)

---
+++ src/mocks/data/company.ts
@@ -0,0 +1,6 @@
+export const MOCK_COMPANY: Company = {
+  id: "mock_company_id",
+  name: "Cloudflare",
+  advertisementLvl: "max",
+  img: "https://static.sinfo.org/static%2F30-sinfo%2FcompanyLogos%2FCloudFlare-01.webp",
+};
