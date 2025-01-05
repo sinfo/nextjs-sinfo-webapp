ebaf16c4449206a53a53ad50790aaf75be927b93
Nuno Alves <nuno.alves02@gmail.com>
2025-01-05 06:49:13 +0000

    Refactor tiles to use list/grid card

--- src/components/session/SessionTile.tsx
+++ src/components/session/SessionTile.tsx
@@ -1,11 +1,10 @@
-import { trimText } from "@/utils/utils";
-import Image from "next/image";
+import ListCard from "@/components/ListCard";
 
 interface SesionTileProps {
-  img: string;
+  img?: string;
   title: string;
   time: string;
-  presenter: Speaker | Company | null;
+  presenter?: Speaker | Company | null;
   type: string;
 }
 
@@ -17,26 +16,12 @@ export function SessionTile({
   type,
 }: SesionTileProps) {
   return (
-    <div className="w-full bg-white rounded-md shadow-md flex flex-row justify-start gap-2 items-center text-xs overflow-hidden px-4 py-2">
-      <Image
-        className="rounded-full"
-        width={40}
-        height={40}
-        src={img}
-        alt={`${presenter?.name && "Presenter"} image`}
-      />
-      <div className="w-full flex flex-col gap-0.5">
-        <div className="flex flex-row items-center justify-between">
-          <div>{time}</div>
-          <div className="bg-blue-dark text-white rounded-md px-2 py-0.5">
-            {type}
-          </div>
-        </div>
-        <div title={title} className="text-sm">
-          {trimText(title, 35)}
-        </div>
-        <div>{presenter ? presenter.name : "Unknown"}</div>
-      </div>
-    </div>
+    <ListCard
+      title={title}
+      subtitle={presenter ? presenter.name : "Unknown"}
+      img={img}
+      headtext={time}
+      label={type}
+    />
   );
 }
