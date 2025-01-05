b98f415855ba0f675f736b9522cc8d48fb702823
Nuno Alves <nuno.alves02@gmail.com>
2025-01-05 02:55:37 +0000

    Add cards' components

---
+++ src/components/GridCard.tsx
@@ -0,0 +1,58 @@
+import Image from "next/image";
+import Link, { LinkProps } from "next/link";
+
+interface GridCardProps {
+  title: string;
+  img: string;
+  imgAltText?: string;
+  extraImage?: string;
+  extraImageAltText?: string;
+  label?: string;
+  link?: string;
+  linkProps?: LinkProps;
+}
+
+export default function GridCard({
+  title,
+  img,
+  imgAltText = "No alt text.",
+  extraImage,
+  extraImageAltText = "No alt text.",
+  label,
+  link,
+  linkProps,
+}: GridCardProps) {
+  return (
+    <Link href={link || "#"} {...linkProps}>
+      <div className="w-[160px] min-h-[240px] flex flex-col items-center justify-between px-4 py-4 gap-y-2 text-sm bg-white rounded-md shadow-md text-center overflow-hidden hover:bg-slate-50 hover:shadow-sm active:bg-gray-200 active:shadow-none">
+        {/* TODO: Fix large names! Truncate is not working */}
+        <span title={title} className="truncate">
+          {title}
+        </span>
+        <Image
+          className="h-full w-full object-contain"
+          width={100}
+          height={100}
+          src={img}
+          alt={imgAltText}
+        />
+        <div className="h-[32px]">
+          {extraImage && (
+            <Image
+              className="m-auto h-full w-full object-contain"
+              width={100}
+              height={32}
+              src={extraImage}
+              alt={extraImageAltText}
+            />
+          )}
+          {label && (
+            <span className="m-auto bg-blue-dark text-white rounded-md px-2 py-1 uppercase">
+              {label}
+            </span>
+          )}
+        </div>
+      </div>
+    </Link>
+  );
+}
---
+++ src/components/ListCard.tsx
@@ -0,0 +1,65 @@
+import Image, { ImageProps } from "next/image";
+import Link, { LinkProps } from "next/link";
+
+interface ListCardProps {
+  img?: string;
+  imgAltText?: string;
+  imgProps?: ImageProps;
+  title: string;
+  subtitle?: string;
+  headtext?: string;
+  label?: string;
+  link?: string;
+  linkProps?: LinkProps;
+}
+
+export default function ListCard({
+  title,
+  img,
+  imgAltText = "No alt text.",
+  subtitle,
+  headtext,
+  label,
+  link,
+  imgProps,
+  linkProps,
+}: ListCardProps) {
+  return (
+    <Link href={link || "#"} {...linkProps}>
+      <div className="min-w-[340px] min-h-[74px] px-4 py-2 flex items-center justify-start gap-x-4 bg-white rounded-md shadow-md text-sm overflow-hidden hover:bg-slate-50 hover:shadow-sm active:bg-gray-200 active:shadow-none">
+        {img && (
+          <Image
+            className="rounded-full"
+            width={40}
+            height={40}
+            src={img}
+            alt={imgAltText}
+            {...imgProps}
+          />
+        )}
+        <div className="flex flex-col justify-start min-w-0">
+          <div className="flex flex-row items-center justify-start gap-x-2 text-xs">
+            {headtext && (
+              <span className="text-gray-500 truncate" title={headtext}>
+                {headtext}
+              </span>
+            )}
+            {label && (
+              <span className="bg-blue-dark text-white rounded-md px-2 py-0.5 uppercase">
+                {label}
+              </span>
+            )}
+          </div>
+          <span className="truncate" title={title}>
+            {title}
+          </span>
+          {subtitle && (
+            <span className="text-xs truncate" title={subtitle}>
+              {subtitle}
+            </span>
+          )}
+        </div>
+      </div>
+    </Link>
+  );
+}
