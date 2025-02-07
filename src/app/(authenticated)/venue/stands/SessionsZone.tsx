9bd76d5a1cab7194ec73a04d9b31d1afd066f5f0
Francisca105 <franciscaalmeida023@gmail.com>
2025-02-07 13:17:34 +0000

    Refactor CompanyStand and SessionsZone components to remove unused imports and wrap room elements in anchor tags for navigation

--- src/app/(authenticated)/venue/stands/SessionsZone.tsx
+++ src/app/(authenticated)/venue/stands/SessionsZone.tsx
@@ -4,52 +4,58 @@ const SessionsStands = () => {
       {/* Main Rooms */}
       <g id="rooms">
         {/* Auditorium */}
-        <g id="auditorium">
-          <rect
-            x="51.5"
-            y="12"
-            width="204.7"
-            height="109"
-            fill="white"
-            stroke="black"
-            strokeWidth="1.5"
-          />
-          <text x="120" y="69" fontSize="13">
-            Auditorium
-          </text>
-        </g>
+        <a href="/schedule?place=Auditorium">
+          <g id="auditorium">
+            <rect
+              x="51.5"
+              y="12"
+              width="204.7"
+              height="109"
+              fill="white"
+              stroke="black"
+              strokeWidth="1.5"
+            />
+            <text x="120" y="69" fontSize="13">
+              Auditorium
+            </text>
+          </g>
+        </a>
 
         {/* Room 1 */}
-        <g id="room1">
-          <rect
-            x="254.9"
-            y="12"
-            width="65"
-            height="109"
-            fill="white"
-            stroke="black"
-            strokeWidth="1.5"
-          />
-          <text x="264" y="69" fontSize="13">
-            Room 1
-          </text>
-        </g>
+        <a href="/schedule?place=Room 1">
+          <g id="room1">
+            <rect
+              x="254.9"
+              y="12"
+              width="65"
+              height="109"
+              fill="white"
+              stroke="black"
+              strokeWidth="1.5"
+            />
+            <text x="264" y="69" fontSize="13">
+              Room 1
+            </text>
+          </g>
+        </a>
 
         {/* Room 2 */}
-        <g id="room2">
-          <rect
-            x="358.8"
-            y="12"
-            width="65"
-            height="109"
-            fill="white"
-            stroke="black"
-            strokeWidth="1.5"
-          />
-          <text x="366" y="69" fontSize="13">
-            Room 2
-          </text>
-        </g>
+        <a href="/schedule?place=Room 2">
+          <g id="room2">
+            <rect
+              x="358.8"
+              y="12"
+              width="65"
+              height="109"
+              fill="white"
+              stroke="black"
+              strokeWidth="1.5"
+            />
+            <text x="366" y="69" fontSize="13">
+              Room 2
+            </text>
+          </g>
+        </a>
       </g>
 
       {/* 2nd Stage */}
