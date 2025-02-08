5046ee5877359024fb119f04577d31f2d41aecf2
Francisca105 <franciscaalmeida023@gmail.com>
2025-02-08 01:29:58 +0000

    Added to the Company page navigation links to venue

--- src/components/EventDayButton.tsx
+++ src/components/EventDayButton.tsx
@@ -9,19 +9,21 @@ interface EventDayButtonProps {
   date: string;
   onClick?: () => void;
   selected?: boolean;
+  disabled?: boolean;
 }
 
 export default function EventDayButton({
   date,
   onClick,
   selected = false,
+  disabled = false,
 }: EventDayButtonProps) {
   return (
     <button
       className="flex flex-col items-center text-center gap-y-1 py-1"
       title={getEventFullDate(date)}
       onClick={onClick}
-      disabled={!onClick}
+      disabled={disabled}
     >
       <span
         className={`flex items-center justify-center font-mono rounded-full w-10 h-10 shadow-md focus:shadow-none ${selected ? "text-sinfo-primary outline outline-sinfo-primary hover:bg-slate-50" : "text-white bg-sinfo-primary"}`}
