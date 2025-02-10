ca287509568e6e11983dd929c0425f2384039e96
Guilherme Baracho <74671552+GuiBaracho@users.noreply.github.com>
2025-02-10 02:57:44 +0000

    Redeem secret achievements

--- src/services/AchievementService.ts
+++ src/services/AchievementService.ts
@@ -30,5 +30,25 @@ export const AchievementService = (() => {
     return null;
   };
 
-  return { getAchievements, getAchievementBySession };
+  const redeemSecretAchievement = async (
+    cannonToken: string,
+    code: string,
+  ): Promise<Achievement | null> => {
+    try {
+      const resp = await fetch(`${achievementsEndpoint}/session/secret`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${cannonToken}`,
+        },
+        body: JSON.stringify({code}),
+      });
+      if (resp.ok) return (await resp.json()) as Achievement;
+    } catch (e) {
+      console.error(e);
+    }
+    return null;
+  }
+
+  return { getAchievements, getAchievementBySession, redeemSecretAchievement };
 })();
