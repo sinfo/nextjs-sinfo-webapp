08d59fc54cbe04a2f13d25b3d6752e21585d4ecb
Leonor Guedes <leonorecaguedes@gmail.com>
2026-04-06 10:58:42 +0100

    fix: prettier add

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -26,9 +26,7 @@ export default async function AuthenticatedLayout({
   return (
     <div className="min-h-dvh text-white flex flex-col">
       <Toolbar />
-      <div className="flex-1 bg-gray-100 text-black flex">
-        {children}
-      </div>
+      <div className="flex-1 bg-gray-100 text-black flex">{children}</div>
       <BottomNavbar />
     </div>
   );
