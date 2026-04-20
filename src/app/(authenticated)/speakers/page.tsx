28f649d91b05269811758aa6b52030fada633c79
Miguel Sanches <A49337@alunos.isel.pt>
2026-04-20 12:07:06 +0100

    fix: speaker cecilia filtered out of the speakers page

--- src/app/(authenticated)/speakers/page.tsx
+++ src/app/(authenticated)/speakers/page.tsx
@@ -4,13 +4,16 @@ import BlankPageWithMessage from "@/components/BlankPageMessage";
 
 export default async function Speakers() {
   let speakers = await SpeakerService.getSpeakers();
+  const CECILIA_ID = "69e5e2c4868a84c5e45715a1";
 
   if (!speakers) {
     return <BlankPageWithMessage message="No speakers found!" />;
   }
 
   // Sort speakers by name
-  speakers = speakers.sort((a, b) => a.name.localeCompare(b.name));
+  speakers = speakers
+    .filter((s) => s.id !== CECILIA_ID) // filter Cecilia out of the speakers page
+    .sort((a, b) => a.name.localeCompare(b.name));
 
   return (
     <div className="container mx-auto">
